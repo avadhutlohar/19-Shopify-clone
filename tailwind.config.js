9baaa32662a1b6165f552cc65770b455c1d3d5b5
avadhutlohar <loharavdhoot@gmail.com>
2022-08-18 10:29:57 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export const content = ["./**/*.html"];
+export const theme = {
+    extend: {
+        colors: {
+            "s-green": "#008060",
+            "s-green-dark": "#004c3f",
+        },
+        screens: {
+            lg: "1080px",
+            md: "750px",
+            1440: "1440px",
+        },
+    },
+};
+export const plugins = [];
